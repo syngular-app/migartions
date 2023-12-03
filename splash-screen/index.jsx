c1a448e292f8e0712d701a6d7ea7a3e7b3746e80
Gabriel Lucena <82548571+AlbuquerqueGabriel20@users.noreply.github.com>
2023-12-02 21:40:15 -0300

    Update index.jsx

--- splash-screen/index.jsx
+++ splash-screen/index.jsx
@@ -1,1 +1,10 @@
-// test page
+import React from "react";
+import "./style.css";
+
+export const SplashScreen = () => {
+  return (
+    <div className="splash-screen">
+      <img className="logo" alt="Logo" src="logo.png" />
+    </div>
+  );
+};
