90ec63967e776166c8f71a93951d9f40242c36e9
Gabriel Lucena <82548571+AlbuquerqueGabriel20@users.noreply.github.com>
2023-12-03 00:29:35 -0300

    Create aviso.tsx

---
+++ settings/aviso.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+export const Aviso = (): JSX.Element => {
+  return (
+    <div className="aviso">
+      <div className="div">
+        <p className="text-wrapper">Tem certeza que deseja sair de sua conta?</p>
+        <div className="overlap-group">
+          <div className="text-wrapper-2">Sim</div>
+        </div>
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="text-wrapper-3">Não</div>
+        </div>
+      </div>
+    </div>
+  );
+};
